6977c237cb0e847d50e9338708f5f41742bb0899
Kostiantyn Spyrydonov <kostiantynspyrydonov@gmail.com>
2026-02-28 11:40:44 +0100

    add all the necessary functionality to the project

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 function ToDoList() {
 
-  const [tasks, setTasks] = useState(["Eat breakfast", "Take a shower", "Walk the dog"]);
+  const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
@@ -10,19 +10,33 @@ function ToDoList() {
   }
 
   function addTask() {
-
+    if (newTask.trim() !== "") {
+      setTasks(t => [...t, newTask]);
+      setNewTask("");
+   }
   }
 
   function deleteTask(index) {
-
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
   }
 
   function moveTaskUp(index) {
-
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+        [updatedTasks[index], updatedTasks[index - 1]] =
+        [updatedTasks[index - 1], updatedTasks[index]];
+        setTasks(updatedTasks);
+    }
   }
 
   function moveTaskDown(index) {
-
+      if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+        [updatedTasks[index], updatedTasks[index + 1]] =
+        [updatedTasks[index + 1], updatedTasks[index]];
+        setTasks(updatedTasks);
+    }
   }
 
   return (
